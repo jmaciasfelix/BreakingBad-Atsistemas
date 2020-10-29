edcaad0aaf2431e5165ae1c068dfa10fca2dafd4
Jesus Macias <maciasfelixj@gmail.com>
2020-10-29 19:22:59 +0100

    🎉 Initial project | 📦 Install npm axios and node-sass

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+/*
+describe('NAME SUITE', () => {
+  test('should return XXXX if receive YYYY', () => {
+    const expected = 'XXXX';
+    const result = 'ZZZZ';
+    expect(result).toBe(expected);
+  });
+});
+*/
+describe('Test App.js', () => {
+  test('should return one if receive one', () => {
+    render(<App />);
+    const result = screen.getByText('App');
+    expect(result).toBeInTheDocument();
+  });
 });
