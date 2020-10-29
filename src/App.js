1901feac3364b2f486a87c7376b9c2b57875b791
Jesus Macias <maciasfelixj@gmail.com>
2020-10-29 20:37:22 +0100

    feat: Implement react router in App and Home component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import './App.scss';
 import Logo from './logo.png';
-import {Home} from "pages/Home"
+//component
+import { Home } from 'pages/Home';
+//react-router-dom
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <div className="nav-container">
-        <figure>
-          <img alt="Breaking Bad logo" src={Logo} />
-        </figure>
-        <p>Buscador 🔍</p>
-      </div>
-      <Home />
+      <Router>
+        <div className="nav-container">
+          <Link to="/">
+            <figure>
+              <img alt="Breaking Bad logo" src={Logo} />
+            </figure>
+          </Link>
+          <p>Buscador 🔍</p>
+        </div>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
+//react-router-dom
+import { Link } from 'react-router-dom';
 
 export const Home = () => (
   <>
     <div className="container-home">
-      <h2>Temporadas 🎥</h2>
-      <h2>Personajes 🧑‍🤝‍🧑</h2>
-      <h2>Asesinos 🔪</h2>
+      <Link to={`temporadas`}>
+        <h2>Temporadas 🎥</h2>
+      </Link>
+      <Link to={`personajes`}>
+        <h2>Personajes 🧑‍🤝‍🧑</h2>
+      </Link>
+      <Link to={`asesinos`}>
+        <h2>Asesinos 🔪</h2>
+      </Link>
     </div>
   </>
 );
-
-Home.propTypes = {};
