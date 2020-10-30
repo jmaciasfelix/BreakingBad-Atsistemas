9aee0e06ddfee3ee1d2715eb4efb5b1b5a7f60f7
Jesus Macias <maciasfelixj@gmail.com>
2020-10-30 19:32:47 +0100

    feat: Create Spinner and list characters in grid

--- src/components/ListOfCharacters/index.js
+++ src/components/ListOfCharacters/index.js
@@ -4,19 +4,25 @@ import PropTypes from 'prop-types';
 //components
 import { Character } from '../Character';
 import { useCharacters } from 'hooks/useCharacters';
+import { Spinner } from 'components/Spinner';
 
 export const ListOfCharacters = () => {
   const [loading, characters] = useCharacters();
 
   return (
     <div className="ListOfCharacters">
-      {characters && characters.map((character) => (
-        <Character
-          key={character.id}
-          name={character.name}
-          img={character.img}
-        />
-      ))}
+      {loading ? (
+        <Spinner />
+      ) : (
+        characters &&
+        characters.map((character) => (
+          <Character
+            key={character.id}
+            name={character.name}
+            img={character.img}
+          />
+        ))
+      )}
     </div>
   );
 };
