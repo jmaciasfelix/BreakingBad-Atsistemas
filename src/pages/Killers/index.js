69ab62e51707826a5f696a1d56204ad0372c521c
Jesus Macias <maciasfelixj@gmail.com>
2020-11-01 17:43:07 +0100

    feat: sort death count

--- src/pages/Killers/index.js
+++ src/pages/Killers/index.js
@@ -3,6 +3,16 @@ import React, { useEffect, useState } from 'react';
 import { getDeaths } from 'services/getDeaths';
 import { getCharacters } from 'services/getCharacters';
 
+const sortDeathCount = (a, b) => {
+  if (a.deathCount > b.deathCount) {
+    return -1;
+  }
+  if (a.deathCount < b.deathCount) {
+    return 1;
+  }
+  return 0;
+};
+
 export const KillersPage = () => {
   const [deathCount, setDeathCount] = useState([]);
 
@@ -11,10 +21,16 @@ export const KillersPage = () => {
       const nameCharacter = response.map(({ name }) =>
         name.replaceAll(' ', '+')
       );
-      getDeaths(nameCharacter).then((res) => setDeathCount(res));
+      getDeaths(nameCharacter).then((res) => {
+        setDeathCount(
+          res
+            .sort((a, b) => sortDeathCount(a, b))
+            .filter(({ deathCount }) => deathCount !== 0)
+        );
+      });
     });
   }, []);
-  
+
   return (
     <>
       <section className="my-5">
