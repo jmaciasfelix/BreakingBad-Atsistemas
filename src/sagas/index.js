15684232ea4f781d81b224e7bbe7a5811fd6f246
Jesus Macias <maciasfelixj@gmail.com>
2020-11-03 12:55:01 +0100

    fix: Fix sagas

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,7 +1,10 @@
-import { all, spawn } from 'redux-saga/effects';
+import { all, fork } from 'redux-saga/effects';
 import search from './search';
 import killers from './killers';
 
 export default function* rootSaga() {
-  yield all([search, killers]);
+  yield all([
+    fork(search),
+    fork(killers)
+  ]);
 }
